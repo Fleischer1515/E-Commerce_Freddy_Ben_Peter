e47e077959e6799570ca9cc989d9cde847484fb4
Fleischer1515 <147046523+Fleischer1515@users.noreply.github.com>
2024-05-08 17:08:37 +0200

    added architecture for databasse

---
+++ database.js
@@ -0,0 +1,30 @@
+const sqlite3 = require('sqlite3').verbose();
+
+// Verbindung zur SQLite-Datenbank herstellen
+const db = new sqlite3.Database('hier name der .db Datei @Ben', (err) => {
+  if (err) {
+    console.error('Fehler beim Öffnen der Datenbank:', err.message);
+  } else {
+    console.log('Erfolgreich mit der SQLite-Datenbank verbunden');
+  }
+});
+
+// Beispielabfrage an die SQLite-Datenbank senden
+db.serialize(() => {
+  db.each('SELECT * FROM mytable', (err, row) => {
+    if (err) {
+      console.error('Fehler beim Ausführen der Abfrage:', err.message);
+    } else {
+      console.log('Abfrageergebnis:', row);
+    }
+  });
+});
+
+// Verbindung zur SQLite-Datenbank beenden
+db.close((err) => {
+  if (err) {
+    console.error('Fehler beim Schließen der Datenbank:', err.message);
+  } else {
+    console.log('Verbindung zur SQLite-Datenbank erfolgreich beendet');
+  }
+});
